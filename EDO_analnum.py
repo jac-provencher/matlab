2924efd375eb31b51cf464b248a19842e662664a
JacobProvencher <jacob.provencher.1@ulaval.ca>
2020-04-11 09:33:31 -0400

    manque juste Taylor

--- EDO_analnum.py
+++ EDO_analnum.py
@@ -2,9 +2,56 @@ import math
 import numpy as np
 import sympy as sp
 
-VARIABLES = x, y, z = sp.symbols('x y z')
-NMAX = 20
+VARIABLES = x, y, z, t = sp.symbols('x y z t')
+NMAX = 10
 TOL = 10**-8
+PRECISION_MACHINE = 10**-15
+
+def newtonZero(nMax, x0, fct, variable):
+
+    f = sp.lambdify(x, fct)
+    fprime = sp.lambdify(variable, fct.diff(variable))
+    x1 = x0 - f(x0)/fprime(x0)
+    i = 0
+
+    while abs(x1 - x0)/(x1 + PRECISION_MACHINE) >= TOL and i <= nMax:
+
+        x0 = x1
+        x1 = x0 - f(x0)/fprime(x0)
+        i += 1
+        print(f"ITERATION {i}\nApproximation:\n{x1:.16e}\n")
+
+def newtonSystem(x, functions, TOL, nMax, variables):
+
+    J = sp.lambdify([variables], sp.Matrix([[f.diff(variable) for variable in variables] for f in functions]))
+    R = sp.lambdify([variables], sp.Matrix([[f] for f in functions]))
+    dx = np.zeros((3, 1))
+    n = 0
+
+    while np.linalg.norm(residu := R(x)) >= TOL and n < nMax:
+
+        jacobien = J(x)
+        dx = np.linalg.solve(jacobien, -residu)
+        x = np.array(x).reshape((len(x), 1)) + dx
+
+        if np.linalg.norm(dx)/(np.linalg.norm(x) + PRECISION_MACHINE) < TOL:
+            break
+        else:
+            n += 1
+            print(f"ITERATION {n}\nJacobien:\n{jacobien}\ndx:\n{dx}\nRésidu:\n{residu}\nx:\n{x}\n")
+            x = x.reshape(len(x))
+
+x0 = 0
+fct = sp.exp(-x) - x
+newtonZero(NMAX, x0, fct, x)
+
+f2 = 2*x**2 - x*y + 3*y - 11
+f1 = x**2 + 2*y**2 - 22
+f3 = x**z + y**(2*x) - z/y
+functions = [f1, f2, f3] # Une liste des fonctions
+xVector = (1, 2, 3) # Approximation initiale x0
+variables = (x, y, z) # Variables utilisées
+newtonSystem(xVector, functions, TOL, NMAX, variables)
 
 class differential_equation_solver:
 
@@ -17,12 +64,23 @@ class differential_equation_solver:
 
     def displayValues(self, data):
 
+        print(f"{'i':^5}|{'t':^9}|{'Valeurs':^29}")
         for iteration, values in data.items():
             for t, y in values.items():
-                print(f"{iteration:<4} |  {t:>6.2f}  |  {y}")
+                print(f"{iteration:<4} |  {t:>5.2f}  |  {y}")
 
-    def Taylor(self):
-        pass
+    def eulerExplicite(self):
+
+        tn, yn = self.t0, np.array(self.y0)
+        data = {0: {tn : yn}}
+        for n in range(1, self.maxIteration+1):
+            data[n] = {}
+            yn = yn + self.h*self.fonctions(tn, yn)
+            tn += self.h
+
+            data[n][tn] = yn
+
+        return data
 
     def eulerModifie(self):
 
@@ -73,18 +131,8 @@ class differential_equation_solver:
 
         return data
 
-    def eulerExplicite(self):
-
-        tn, yn = self.t0, np.array(self.y0)
-        data = {0: {tn : yn}}
-        for n in range(1, self.maxIteration+1):
-            data[n] = {}
-            yn = yn + self.h*self.fonctions(tn, yn)
-            tn += self.h
-
-            data[n][tn] = yn
-
-        return data
+    def Taylor(self):
+        pass
 
 f1 = lambda t, y: y[1]
 f2 = lambda t, y: -30*(y[0] + y[1])
@@ -92,50 +140,6 @@ fonctions = lambda t, y: np.array([f1(t, y), f2(t, y)])
 condition_initiale = (0, 0, 30)
 h = 0.01
 solver = differential_equation_solver(h, NMAX, condition_initiale, fonctions)
-# data = solver.eulerExplicite()
-# solver.displayValues(data)
-
-def newtonZero(nMax, x0, fct):
-
-    TOL = 10**-10
-    err_machine = 10**-15
-    f = sp.lambdify(x, fct)
-    fprime = sp.lambdify(x, fct.diff(x))
-    x1 = x0 - f(x0)/fprime(x0)
-    i = 0
-
-    while abs(x1 - x0)/(x1 + err_machine) >= TOL and i <= nMax:
-
-        x0 = x1
-        x1 = x0 - f(x0)/fprime(x0)
-        i += 1
-
-    return x1
-
-x0 = 0
-fct = sp.exp(-x) - x
-# print(newtonZero(NMAX, x0, fct))
-
-def newtonSystem(x, functions, TOL, nMax):
-
-    J = sp.lambdify([VARIABLES], sp.Matrix([[f.diff(variable) for variable in VARIABLES] for f in functions]))
-    R = sp.lambdify([VARIABLES], sp.Matrix([[f] for f in functions]))
-    dx = np.zeros((3, 1))
-    n = 0
-
-    while np.linalg.norm(residu := R(x)) >= TOL and n < nMax:
-        jacobien = J(x)
-        dx = np.linalg.solve(jacobien, -residu)
-        x = np.array(x).reshape((len(x), 1)) + dx
-        print(f"ITERATION {n+1}\nJacobien:\n{jacobien}\ndx:\n{dx}\nRésidu:\n{residu}\nx:\n{x}\n")
-        x = x.reshape(len(x))
-
-        n += 1
-
-f2 = 2*x**2 - x*y + 3*y - 11
-f1 = x**2 + 2*y**2 - 22
-f3 = x**z + y**(2*x) - 43**z
-functions = [f1, f2, f3]
-x = (1, 2, 6)
-newtonSystem(x, functions, TOL, NMAX)
+data = solver.eulerExplicite()
+solver.displayValues(data)
 
